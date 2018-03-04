180e8000f2546e84bf2bd1f9a4d4608984a29f60
Scotow <contact@scotow.com>
2018-03-04 14:59:30 +0100

    p02

---
+++ p02_scotow.swift
@@ -0,0 +1,11 @@
+extension List {
+
+    /// P02 (*) Find the last but one element of a linked list.
+    /// - returns: T - second to last element of the linked list
+    /// - author: Benjamin Lopez (contact@scotow.com)
+    /// - complexity: O(n)
+    var pennultimate: T? {
+        return (nextItem != nil && nextItem?.nextItem == nil) ? value : nextItem?.pennultimate
+    }
+
+}
