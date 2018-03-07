de60a6ab2ed37198c8a21061d6e671658bc9edd9
James Baxter <james.baxter@focusrite.com>
2018-03-07 17:40:59 +0000

    New p05 that returns a new instance of List<T>, rather than reversing in-place

--- Solutions/tehpsy/p05_tehpsy.swift
+++ Solutions/tehpsy/p05_tehpsy.swift
@@ -2,36 +2,18 @@ import NinetyNineSwiftProblems
 
 extension List {
 
-    /// P05 (*) Reverses a linked list in-place.
-    /// - returns: Void
-    /// - author: James Baxter (jimmehb@gmail.com)
-    /// - complexity: O(n)
-    public func reverse() {
-
-        var oldList: List<T>? = self
-        var newList: List<T>? = self.reversed()
-
-        oldList = self
-        repeat {
-            oldList!.value    = newList!.value
-            oldList!.nextItem = newList!.nextItem
-            oldList = oldList!.nextItem
-            newList = newList!.nextItem
-        } while oldList != nil
-    }
-
     /// P05 (*) Reverses a linked list.
-    /// - returns: New instance List<T> of original List<T>, with values reversed
+    /// - returns: New instance List<T> with values reversed
     /// - author: James Baxter (jimmehb@gmail.com)
     /// - complexity: O(n)
-    public func reversed() -> List<T> {
+    public func reverse() -> List<T> {
 
         var oldList: List<T>? = self
         var newList: List<T>? = nil
         var previousNewListItem: List<T>? = nil
 
         repeat {
-            let newListItem: List<T> = List(oldList!.value)!
+            let newListItem: List<T> = List(oldList!.value)
             newListItem.nextItem = previousNewListItem
             newList = newListItem
 
