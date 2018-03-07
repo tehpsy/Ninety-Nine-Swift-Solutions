34005d69f8f7f562698072445a48c8256036e47b
Eneko Alonso <eneko.alonso@gmail.com>
2018-03-06 21:11:02 -0800

    Imports

--- Solutions/scotow/p02_scotow.swift
+++ Solutions/scotow/p02_scotow.swift
@@ -1,11 +1,13 @@
+import NinetyNineSwiftProblems
+
 extension List {
 
     /// P02 (*) Find the last but one element of a linked list.
     /// - returns: T - second to last element of the linked list
     /// - author: Benjamin Lopez (contact@scotow.com)
     /// - complexity: O(n)
-    var pennultimate: T? {
-        return (nextItem != nil && nextItem?.nextItem == nil) ? value : nextItem?.pennultimate
+    var penultimate: T? {
+        return (nextItem != nil && nextItem?.nextItem == nil) ? value : nextItem?.penultimate
     }
 
 }
