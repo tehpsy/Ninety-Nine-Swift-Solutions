87f940cfe327bd2fbf1902ab33fc62c16ff77067
Alex Marchis <alexandru.marchis@ibm.com>
2018-03-12 15:56:46 +0200

    Remove typo from P06

--- Solutions/alexmarchis1990/p06_alexmarchis1990.swift
+++ Solutions/alexmarchis1990/p06_alexmarchis1990.swift
@@ -3,7 +3,7 @@ import NinetyNineSwiftProblems
 extension List where T:Equatable {
     
     func isPalindrome() -> Bool {
-        §
+        
         let loopLength = (self.length - -1)/2
         for i in 0...loopLength {
             if self[i] != self[self.length-i-1] {
